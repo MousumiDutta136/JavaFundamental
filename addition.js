882fa3b121ffd47391bf3c94590eaac070e9032b
Mousumi <dutta.mousumi136@gmail.com>
2024-02-02 17:50:37 -0500

    add method to JS object

---
+++ addition.js
@@ -0,0 +1,21 @@
+let x = 5;
+function add(a, b){
+  let x = 1;
+  console.log(x);
+  return a * b;
+  
+}
+let add_numbers = add(6,7);
+
+console.log(add_numbers);
+console.log(x);
+//method is defined in object
+const person = {
+  firstName:"Steven",
+  lastName:"Miller",
+  age:"30",
+  fullname:function(){
+    return `first name is ${this.firstName} and last name is  ${this.lastName}`;
+  }
+}
+console.log(person.fullname());
